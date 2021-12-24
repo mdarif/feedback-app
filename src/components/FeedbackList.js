301bd76c46096cb91eadd7dbbd00c85d0cea4542
Mohammad Arif <arif_iq@yahoo.co.in>
2021-12-24 18:35:20 +0530

    Added concurrently, json-server, proxy, loader, and all the data is almost dynamic now with the help of fake REST API (json-server), the project is almost completed.

--- src/components/FeedbackList.js
+++ src/components/FeedbackList.js
@@ -2,16 +2,19 @@ import { motion, AnimatePresence } from 'framer-motion'
 import { useContext } from 'react'
 import FeedbackItem from './FeedbackItem'
 import FeedbackContext from '../context/FeedbackContext'
+import Spinner from './shared/Spinner'
 
 function FeedbackList () {
-  const { feedback } = useContext(FeedbackContext)
+  const { feedback, isLoading } = useContext(FeedbackContext)
 
-  console.log('feedback', feedback)
-  if (!feedback || feedback.length === 0) {
+  console.log('isLoading', isLoading)
+  if (!isLoading && (!feedback || feedback.length === 0)) {
     return <p>No feedback yet</p>
   }
 
-  return (
+  return isLoading ? (
+    <Spinner />
+  ) : (
     <div className='feedback-list'>
       <AnimatePresence>
         {feedback.map(item => (
